0aa67f4d0cfb944130afa1027d25c4894c749400
Itzelsanchezz <72948547+Itzelsanchezz@users.noreply.github.com>
2022-02-14 17:21:24 -0600

    worked on goal 1 day 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,19 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+    <div className="app">
+      Trivia! <Question />{" "}
+    </div>
+  );
+  var currentQuestion = 0;
+}
+
+// What is a Question Properties? Answers, Question
+// What Components Make up the Question? Anwser
+// What States are part of the question?  Unanswered/Answered
+function Question(props) {
+  return <div>this is a question</div>;
 }
 
 export default App;
